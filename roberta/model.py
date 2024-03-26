9b05054d26ffe623da281eecdd1e781f50be4af6
Liu-KM <2036804530@qq.com>
2024-03-26 17:03:34 +0800

    remove useless code

--- roberta/model.py
+++ roberta/model.py
@@ -10,10 +10,6 @@ from peft import (
 )
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, get_linear_schedule_with_warmup, set_seed
 
-model_cfg = {
-    "model_name_or_path": "roberta-large",
-    "peft_config": LoraConfig(task_type="SEQ_CLS", inference_mode=False, r=8, lora_alpha=16, lora_dropout=0.1),
-}
 
 def get_model(model_cfg):
     model = AutoModelForSequenceClassification.from_pretrained(model_cfg.model_name_or_path, return_dict=True)
